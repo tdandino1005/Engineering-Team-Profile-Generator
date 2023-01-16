aac5414cb55ca21f27154caad3a103089bccd7a1
Tony Dandino <tdandino1005@gmail.com>
2023-01-15 19:11:44 -0600

    added comments to employee.js

--- src/models/employee.js
+++ src/models/employee.js
@@ -4,19 +4,19 @@ class Employee {
         this.id = id;
         this.email = email;
     }
-
+// returning name
     getName() {
         return this.name;
     }
-
+// returning ID
     getId() {
         return this.id;
     }
-
+// returning email
     getEmail() {
         return this.email;
     }
-
+// returning employee
     getRole() {
         return "Employee";
     }
